5aa1d9ba6934b3cf8a7377cd95a239c7eae5f3b0
Gareth Nicholson <gareth@io.co.za>
2016-01-15 13:43:14 +0200

    Create invite email to existing user

---
+++ resources/views/invites/emails/invite.blade.php
@@ -0,0 +1,7 @@
+<h3>Hello {{ $user->first_name }}</h3>
+
+<p>You have been invited to join a board on SuntoPluto.</p>
+
+<p>Please click the following <a href="{{ url('/invites') }}">link</a> to view new invite.</p>
+
+<p>Thank you</p>
