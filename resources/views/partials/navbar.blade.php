1f8de8b2ebf3a1cb7605d18b1a9c761c1cc937b9
Gareth Nicholson <gareth@io.co.za>
2016-01-22 11:13:06 +0200

    Create a navbar for site navigation

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">SuntoPluto</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+            @if (auth()->check())
+                <li><a href="/boards">Boards</a></li>
+                @if (count($invites) >= 1)
+                    <li><a href="/invites">Invites</a></li>
+                @endif
+                @if (Request::path() === 'boards' or Request::path() === 'invites') @else
+                    <li><a href="{{ route('tasks.index', ['identifier' => $board->identifier]) }}">{{ $board->name }}</a></li>
+                    @include('partials.reports_dropdown')
+                @endif
+                <li><a href="/logout">logout</a></li>
+            @else
+                <li><a href="/login">login</a></li>
+            @endif
+        </ul>
+    </div>
+</nav>
