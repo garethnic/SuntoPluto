73674722d8812b1268212fc3068b837ecea0fa10
Gareth Nicholson <gareth@io.co.za>
2016-01-22 10:57:35 +0200

    add css file to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,8 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.browserify(['app.js'], 'public/js/app.js');
+
+    mix.styles('app.css', 'public/css');
+
     mix.version('css/app.css');
 });
