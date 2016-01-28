e18714f2198a615bccd0cd082d4d3fe91da0cf7a
Gareth Nicholson <gareth@io.co.za>
2016-01-28 12:22:09 +0200

    Create database files for admin setup

---
+++ app/Admin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Flisk;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use SoftDeletes;
+    
+    /**
+     * The table used
+     *
+     * @var string
+     */
+    protected $table = 'admins';
+
+    /**
+     * The attributes that are mass assignable
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'username',
+        'email',
+        'password',
+        'timezone',
+        'token'
+    ];
+}
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,12 +17,24 @@ $factory->define(Flisk\User::class, function (Faker\Generator $faker) {
         'last_name' => $faker->lastName,
         'gender' => rand(0, 1) ? 'm' : 'f',
         'confirmed' => $faker->boolean(),
-        'Username' => $faker->name,
+        'username' => $faker->name,
         'email' => $faker->email,
         'timezone' => $faker->timezone,
         'password' => bcrypt('password'),
         'remember_token' => str_random(10),
     ];
+});
+
+$factory->define(\Flisk\Admin::class, function (Faker\Generator $faker) {
+   return [
+       'first_name' => $faker->firstName,
+       'last_name' => $faker->lastName,
+       'username' => $faker->name,
+       'email' => $faker->email,
+       'timezone' => $faker->timezone,
+       'password' => bcrypt('password'),
+       'remember_token' => str_random(10),
+   ];
 });
 
 $factory->define(\Flisk\Task::class, function (Faker\Generator $faker) {
